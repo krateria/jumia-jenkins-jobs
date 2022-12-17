03ed407bdf0293d1021749b273aab4aadcda255d
Fernando La Gamba <krateria@gmail.com>
2022-12-17 19:42:34 -0300

    update

--- ecr-push.sh
+++ ecr-push.sh
@@ -1,3 +1,3 @@
 aws ecr --profile jumia get-login-password --region eu-west-1 | docker login --username AWS --password-stdin 992122884453.dkr.ecr.eu-west-1.amazonaws.com
-docker tag jenkins-inbound-node:latest 992122884453.dkr.ecr.eu-west-1.amazonaws.com/jenkins-inbound-node:latest
-docker push 992122884453.dkr.ecr.eu-west-1.amazonaws.com/jenkins-inbound-node:latest
+docker tag jenkins-inbound-node:latest 992122884453.dkr.ecr.eu-west-1.amazonaws.com/jenkins-inbound-agent:latest
+docker push 992122884453.dkr.ecr.eu-west-1.amazonaws.com/jenkins-inbound-agent:latest
