8debb1dd453fe9b287a91c866e40f365d5695775
Fernando La Gamba <krateria@gmail.com>
2022-12-18 19:58:13 -0300

    update

--- docker-build.sh
+++ docker-build.sh
@@ -1,1 +1,2 @@
 sudo docker build -f ./Dockerfile-jenkins-node . -t jenkins-inbound-node:latest
+sudo docker build -f ./Dockerfile-jenkins-jdk8 . -t jenkins-inbound-jdk8:latest
--- ecr-push.sh
+++ ecr-push.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
 aws ecr --profile jumia get-login-password --region eu-west-1 | docker login --username AWS --password-stdin 992122884453.dkr.ecr.eu-west-1.amazonaws.com
-docker tag jenkins-inbound-node:latest 992122884453.dkr.ecr.eu-west-1.amazonaws.com/jenkins-inbound-agent:latest
-docker push 992122884453.dkr.ecr.eu-west-1.amazonaws.com/jenkins-inbound-agent:latest
+docker tag jenkins-inbound-node:latest 992122884453.dkr.ecr.eu-west-1.amazonaws.com/jenkins-inbound-node:latest
+docker tag jenkins-inbound-jdk8:latest 992122884453.dkr.ecr.eu-west-1.amazonaws.com/jenkins-inbound-jdk8:latest
+
+docker push 992122884453.dkr.ecr.eu-west-1.amazonaws.com/jenkins-inbound-node:latest
+docker push 992122884453.dkr.ecr.eu-west-1.amazonaws.com/jenkins-inbound-jdk8:latest
