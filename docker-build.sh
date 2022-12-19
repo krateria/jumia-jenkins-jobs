c4847a5009157324a2b8f3b75d390998520a1d6e
Fernando La Gamba <krateria@gmail.com>
2022-12-18 21:04:58 -0300

    update

--- docker-build.sh
+++ docker-build.sh
@@ -1,2 +1,3 @@
 sudo docker build -f ./Dockerfile-jenkins-node . -t jenkins-inbound-node:latest
 sudo docker build -f ./Dockerfile-jenkins-jdk8 . -t jenkins-inbound-jdk8:latest
+sudo docker build -f ./Dockerfile-jenkins-postgres . -t jenkins-inbound-postgres:latest
--- ecr-push.sh
+++ ecr-push.sh
@@ -3,6 +3,8 @@
 aws ecr --profile jumia get-login-password --region eu-west-1 | docker login --username AWS --password-stdin 992122884453.dkr.ecr.eu-west-1.amazonaws.com
 docker tag jenkins-inbound-node:latest 992122884453.dkr.ecr.eu-west-1.amazonaws.com/jenkins-inbound-node:latest
 docker tag jenkins-inbound-jdk8:latest 992122884453.dkr.ecr.eu-west-1.amazonaws.com/jenkins-inbound-jdk8:latest
+docker tag jenkins-inbound-postgres:latest 992122884453.dkr.ecr.eu-west-1.amazonaws.com/jenkins-inbound-postgres:latest
 
 docker push 992122884453.dkr.ecr.eu-west-1.amazonaws.com/jenkins-inbound-node:latest
 docker push 992122884453.dkr.ecr.eu-west-1.amazonaws.com/jenkins-inbound-jdk8:latest
+docker push 992122884453.dkr.ecr.eu-west-1.amazonaws.com/jenkins-inbound-postgres:latest
